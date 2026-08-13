8829a72f75e1d7d4aa9cd71a445e136a0bc0f7a7
Adam <kaidsalimadam@gmail.com>
2026-08-13 13:29:00 +0100

    finishing the base parsing file

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
 from path import Path
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from enum import Enum
 from typing import Optional, Any, Tuple, Dict, List
 
@@ -10,11 +10,11 @@ class ZoneType(str, Enum):
     PRIORITY = "priority"
 
 
-class Zone(Basemodel):
+class Zone(BaseModel):
     name: str
     x: int
     y: int
-    zone_type: ZoneType = Zonetype.NORMAL
+    zone_type: ZoneType = ZoneType.NORMAL
     color: Optional[str] = None
     max_drones : int = Field(default=1, ge=1)
 
@@ -37,5 +37,27 @@ class Connection(BaseModel):
     zone_name2: str
     max_link_capacity: int = Field(default= 1, ge=1)
     
+
 class Graph(BaseModel):
-    
+    nb_drones : int = Field(default=0, ge=0)
+    start_hub: Optional[Zone] = None
+    end_hub: Optional[Zone] = None
+    zones = Dict[str, Zone] = None
+    connection : List[Connection] = Field(default_factory=list)
+    adj_list: Dict[str, List[Connection]] = Field(default_factory=dict)
+
+    def add_zone(self, zone: Zone) -> None:
+        if zone.name in self.zones:
+            raise ValueError(f"Duplicate zone name '{zone.name}' ")
+        self.zones[zone.name] = zone
+        self.adj_list[zone.name] = []
+
+    def add_connectoion(self, conn: Connection) -> None:
+        if conn.zone1_name not in self.zones or conn.zone2_name not in self.zones:
+            raise ValueError(
+                f"Connection links undefined zone: {conn.zone1_name}"
+                f"or {conn.zone_2name}."
+            )
+        self.connections.append(conn)
+        self.adj_list[conn.zone1_name].append(conn)
+        self.adj_list[conn.zone2_name].append(conn)
